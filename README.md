f0297fac16eb2870cddb5ee076cf917d996a64b8
aproposmath <aproposmath@proton.me>
2026-02-11 19:05:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Stationeers Example Mod Template
+
+This repository is not meant to be used as-is. I use it for my own mods as a git submodule in `Template`, in order to keep the code organized and easily maintainable.
---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+git checkout main
+git pull
+
+cp NuGet.Config ../
+cp github-deploy.yml ../.github/workflows/deploy.yml || true
+
+cd ..
+git add Template
+echo "Update done, to commit, run: git commit -m'Update Template'"
