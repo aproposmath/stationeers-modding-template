498f8d1da0a7da29a45fe7a0740c3be64eb676a7
aproposmath <aproposmath@proton.me>
2026-02-11 20:22:34 +0100

    Rebuild version generator only when necessary

--- update.sh
+++ update.sh
@@ -7,6 +7,10 @@ cp NuGet.Config ../
 cp Main.csproj.template ../Main.csproj
 cp github-deploy.yml ../.github/workflows/deploy.yml || true
 
+# cleanup to trigger rebuild
+rm -r VersionGenerator/bin
+rm -r VersionGenerator/obj
+
 cd ..
 git add Template
 echo "Update done, to commit, run: git commit -m'Update Template'"
