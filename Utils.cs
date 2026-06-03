8a0cc1c2eee1f19c4a40d41abe77612ff6be9b9a
aproposmath <aproposmath@proton.me>
2026-06-03 17:26:31 +0200

    Utility function to detect if a mod was loaded via BepInEx ScriptEngine

---
+++ Utils.cs
@@ -0,0 +1,13 @@
+internal static class Utils
+{
+    public static bool IsLoadedByScriptEngine(Type type)
+    {
+        string assemblyPath = Path.GetFullPath(type.Assembly.Location);
+        string scriptsPath = Path.GetFullPath(Path.Combine(BepInEx.Paths.BepInExRootPath, "scripts"));
+        L.Debug($"Assembly path: {assemblyPath}");
+        L.Debug($"Scripts path: {scriptsPath}");
+        var result = assemblyPath.StartsWith(scriptsPath, StringComparison.OrdinalIgnoreCase);
+        L.Debug($"Is loaded by script engine: {result}");
+        return result;
+    }
+}
