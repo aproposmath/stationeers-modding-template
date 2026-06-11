4e1322488788bbeb445f7c29efa6368326bb8d67
aproposmath <aproposmath@proton.me>
2026-06-12 00:32:58 +0200

    Utility class to detect memory allocations

--- ModUtils.cs
+++ ModUtils.cs
@@ -9,3 +9,36 @@ internal static class ModUtils
         return string.IsNullOrEmpty(type.Assembly.Location);
     }
 }
+
+#if DEBUG
+public readonly struct ScopedMemoryTracker(string file = null, int line = -1) : IDisposable
+{
+    private readonly long StartMemory = GC.GetTotalMemory(false);
+
+    private readonly string _file = file;
+    private readonly int _line = line;
+
+    public static ScopedMemoryTracker Track(
+        [CallerFilePath] string file = "",
+        [CallerLineNumber] int line = 0)
+    {
+        return new ScopedMemoryTracker(file, line);
+    }
+    public void Dispose()
+    {
+        var endMemory = GC.GetTotalMemory(false);
+        var delta = endMemory - StartMemory;
+        if (delta != 0)
+        {
+            var Name = $"{Path.GetFileName(_file)}:{_line}";
+            L.Debug($"\t{Name}\t{delta} bytes ({delta / 1024.0:F2} KB)");
+        }
+    }
+}
+#else
+public readonly struct ScopedMemoryTracker : IDisposable
+{
+    public static ScopedMemoryTracker Track() => new ScopedMemoryTracker();
+    public void Dispose() { }
+}
+#endif
